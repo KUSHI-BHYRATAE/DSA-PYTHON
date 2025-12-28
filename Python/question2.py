111db58548289d4b222bcf3290006975fb2759c5
KUSHI-BHYRATAE <bhyrataekushi@gmail.com>
2025-12-27 20:47:52 -0800

    Add question2 solution

---
+++ Python/question2.py
@@ -0,0 +1,8 @@
+# Taking input for birth year 
+birth_year_string = input(" Enter your  birth year")
+birth_year = int(birth_year_string)
+
+#calculate age
+age = 2025 - birth_year
+# print 
+print("You will be " , age , " years old in 2025!")
