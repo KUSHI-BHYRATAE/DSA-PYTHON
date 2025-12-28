038352b2c51d115c7f60b909e25321ebe97b32a9
KUSHI-BHYRATAE <bhyrataekushi@gmail.com>
2025-12-27 23:10:46 -0800

    Loops + List

---
+++ Python/question15.py
@@ -0,0 +1,11 @@
+numbers = [12, 45, 3, 98, 7, 34, 21]
+for i in numbers:
+  print(i)
+for  i in numbers:
+  if i > 30 :
+    print(i)
+count = 0
+for i in numbers:
+  if i > 30:
+    count += 1
+print(" count of nums greater than 30:" , count )    
