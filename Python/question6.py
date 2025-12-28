22e15c916c6929963787e4112570250960fb5469
KUSHI-BHYRATAE <bhyrataekushi@gmail.com>
2025-12-27 21:47:46 -0800

    if statements

---
+++ Python/question6.py
@@ -0,0 +1,12 @@
+ age = int(input("Enter your age:"))
+if age < 13:
+  print(" You are a child")
+elif 13 <= age <= 17:
+  print("You are a teenager")
+elif 18 <= age <= 64:
+  print(" You are an adult")
+else:
+  print(" you are a Senior")
+  
+
+  
