05f6ef0a446e0c070954e0c7c5877cc738e0db3d
KUSHI-BHYRATAE <bhyrataekushi@gmail.com>
2025-12-27 20:16:50 -0800

    Create question1.py

---
+++ Python/question1.py
@@ -0,0 +1,12 @@
+#Creating  variables of diffrent data types to describe myself 
+
+name = "Kushi Bhyratae"
+age = 18
+height = 1.5
+are_you_student = True 
+# Printing with nice formating 
+print("-----PERSONAL DETAILS-----")
+print(" NAME:",name )
+print("AGE:" , age)
+print(" HEIGHT:" , height)
+print(" Are you a student:" , are_you_student)
