370e0def8ad48bbc6b5f0ccd01789857749171b7
KUSHI-BHYRATAE <bhyrataekushi@gmail.com>
2025-12-27 22:06:30 -0800

    if statements

---
+++ Python/question9.py
@@ -0,0 +1,7 @@
+exam1_score = int(input(" Enter score of first exam:"))
+exam2_score = int(input("Enter score of second exam:"))
+if exam1_score >= 50 and exam2_score >= 50:
+  print("You passed!")
+else:
+  print("You need to retake some exams".)
+  
